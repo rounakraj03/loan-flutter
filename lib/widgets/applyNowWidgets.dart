3ed63d81a2118dd6b9c8bcbdf9495eab8cd73bcb
rounak-stellar <rounakraj@infostellar.com>
2023-08-29 16:00:13 +0530

    implement gender radio button

--- lib/widgets/applyNowWidgets.dart
+++ lib/widgets/applyNowWidgets.dart
@@ -51,7 +51,7 @@ class _ApplyNowFormState extends State<ApplyNowForm> {
     double width = MediaQuery.of(context).size.width;
     return LayoutBuilder(
       builder: (context, constraints) {
-        if(width > Constants.desktop_view){
+        if(width > Constants.desktop_view+150){
           return Container(
             padding: EdgeInsets.symmetric(horizontal: 150, vertical: 80),
             child: Form(
@@ -416,7 +416,225 @@ class _ApplyNowFormState extends State<ApplyNowForm> {
           );
         }
         else {
-          return Container();
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: width*0.1, vertical: 60),
+            child: Form(
+              key: applyUsFormKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Loan Amount (₹)", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:loanAmountController, validatorType: ValidatorType.validateNotNull),
+
+                  SizedBox(height: 30,),
+
+                  Text("Purpose of Loan", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomDropdown(
+                    items: purposeOfLoanOptions.map((e) => DropdownMenuItem(
+                      value: e.toString(),
+                      child: Text(e.toString()),
+                    )).toList(),
+                    onChanged: (value) => setState(() {
+                      if(value != null) {
+                        purposeOfLoanController.text = value;
+                      }
+                    }),
+                  ),
+
+                  SizedBox(height: 30,),
+
+                  Text("First Name", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:firstNameController, validatorType: ValidatorType.validateNotNull),
+
+                  SizedBox(height: 30,),
+
+                  Text("Last Name", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:lastNameController, validatorType: ValidatorType.validateNotNull),
+
+                  SizedBox(height: 30,),
+
+                  Text("Email", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:emailController, validatorType: ValidatorType.validateEmail),
+
+
+                  SizedBox(height: 30,),
+
+                  Text("Mobile Phone", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:phoneController, validatorType: ValidatorType.validateMobile),
+
+
+                  SizedBox(height: 30,),
+
+                  Text("Date of Birth", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:dobController, validatorType: ValidatorType.validateNotNull),
+
+                  SizedBox(height: 30,),
+
+                  Text("Marital Status", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomDropdown(
+                    items: maritalStatusOptions.map((e) => DropdownMenuItem(
+                      value: e.toString(),
+                      child: Text(e.toString()),
+                    )).toList(),
+                    onChanged: (value) => setState(() {
+                      if(value != null) {
+                        maritalStatusController.text = value;
+                      }
+                    }),
+                  ),
+
+
+                  SizedBox(height: 30,),
+
+                  Text("Number of Dependants", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomDropdown(
+                    items: numberOfDependantsOptions.map((e) => DropdownMenuItem(
+                      value: e.toString(),
+                      child: Text(e.toString()),
+                    )).toList(),
+                    onChanged: (value) => setState(() {
+                      if(value != null) {
+                        numberOfDependantsController.text = value;
+                      }
+                    }),
+                  ),
+
+
+
+                  SizedBox(height: 30,),
+
+                  Text("Country", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:countryController, validatorType: ValidatorType.validateNotNull),
+
+                  SizedBox(height: 30,),
+
+                  Text("Town/City", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:cityController, validatorType: ValidatorType.validateNotNull),
+
+
+                  SizedBox(height: 30,),
+
+                  Text("Street", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:streetController, validatorType: ValidatorType.validateNotNull),
+
+
+                  SizedBox(height: 30,),
+
+                  Text("House Name/Number", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:houseNumberController, validatorType: ValidatorType.validateNotNull),
+
+
+                  SizedBox(height: 30,),
+
+                  Text("Home Owner Status", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:homeOwnerController, validatorType: ValidatorType.validateNotNull),
+
+
+                  SizedBox(height: 30,),
+                  Text("Time at Address", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:timeAtAddressController, validatorType: ValidatorType.validateNotNull),
+
+
+                  SizedBox(height: 30,),
+
+                  Text("Employment Status", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomDropdown(
+                    items: employmentStatusOptions.map((e) => DropdownMenuItem(
+                      value: e.toString(),
+                      child: Text(e.toString()),
+                    )).toList(),
+                    onChanged: (value) => setState(() {
+                      if(value != null) {
+                        employmentStatusController.text = value;
+                      }
+                    }),
+                  ),
+
+
+                  SizedBox(height: 30,),
+
+                  Text("Employment Industry", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:employmentIndustryController, validatorType: ValidatorType.validateNotNull),
+
+                  SizedBox(height: 30,),
+
+                  Text("Employer Name", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:employerNameController, validatorType: ValidatorType.validateNotNull),
+
+                  SizedBox(height: 30,),
+
+                  Text("Work Phone Number", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:workPhoneController, validatorType: ValidatorType.validateMobile),
+
+                  SizedBox(height: 30,),
+
+                  Text("Length of Employment", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:lengthOfEmploymentController, validatorType: ValidatorType.validateNotNull),
+
+                  SizedBox(height: 30,),
+
+                  Text("Monthly Income(₹)", style: textstyle1),
+                  SizedBox(height: 5,),
+                  CustomTextField(controller:monthlyIncomeController, validatorType: ValidatorType.validateNotNull),
+
+
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CustomElevatedButton(child: "APPLY NOW", onPressed: (){
+                        if(applyUsFormKey.currentState!.validate()){
+                          print("FORM VALIDATED");
+                          print("loanAmountController => ${loanAmountController.text}");
+                          print("purposeOfLoanController => ${purposeOfLoanController.text}");
+                          print("genderController => ${genderController.text}");
+                          print("firstNameController => ${firstNameController.text}");
+                          print("lastNameController => ${lastNameController.text}");
+                          print("emailController => ${emailController.text}");
+                          print("phoneController => ${phoneController.text}");
+                          print(" dobController => ${dobController.text}");
+                          print("maritalStatusController => ${maritalStatusController.text}");
+                          print("numberOfDependantsController => ${numberOfDependantsController.text}");
+                          print("countryController  => ${countryController.text}");
+                          print("cityController => ${cityController.text}");
+                          print("streetController => ${streetController.text}");
+                          print("houseNumberController => ${houseNumberController.text}");
+                          print("homeOwnerController => ${homeOwnerController.text}");
+                          print("timeAtAddressController => ${timeAtAddressController.text}");
+                          print("employmentStatusController => ${employmentStatusController.text}");
+                          print("employmentIndustryController => ${employmentIndustryController.text}");
+                          print("employerNameController => ${employerNameController.text}");
+                          print("workPhoneController => ${workPhoneController.text}");
+                          print("lengthOfEmploymentController => ${lengthOfEmploymentController.text}");
+                          print("monthlyIncomeController => ${monthlyIncomeController.text}");
+                        }
+                      }, width: 180, height: 60,),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
         }
       },);
   }
