5a4faf17bfc4df8438599fe77ba6d77044ef88e1
rounak <rounakraj0309@gmail.com>
2023-09-09 02:50:42 +0530

    loan done

--- lib/res/assets.dart
+++ lib/res/assets.dart
@@ -2,21 +2,12 @@ class Assets {
   // Images
   static const String logo = "assets/images/logo.png";
   static const String logo2 = "assets/images/logo2.png";
-  static const String smilingMan = "assets/images/smilingMan.png";
-  static const String smilingWomen = "assets/images/smilingWomen.png";
-  static const String manMug = "assets/images/manMug.jpg";
-  static const String homeImage = "assets/images/homeImage.jpg";
   static const String imageOne = "assets/images/imageOne.png";
   static const String imageTwo = "assets/images/imageTwo.png";
   static const String imageThree = "assets/images/imageThree.png";
   static const String imageFour = "assets/images/imageFour.png";
-  static const String imageFrame = "assets/images/frame.jpg";
   static const String imageFrame2 = "assets/images/frame2.png";
   static const String secondWidgetImage = "assets/images/secondWidgetImage.jpg";
-  static const String sliderGirlImage = "assets/images/sliderGirlImage.png";
-  static const String sliderGirlImage2 = "assets/images/sliderGirlImage2.jpg";
-  static const String sliderGirlImage3 = "assets/images/sliderGirlImage3.jpg";
-  static const String sliderGirlImage4 = "assets/images/sliderGirlImage4.jpg";
   static const String sliderGirlImage5 = "assets/images/sliderGirlImage5.jpg";
   static const String dottedArrowUp = "assets/images/dottedArrowUp.png";
   static const String dottedArrowDown = "assets/images/dottedArrowDown.png";
@@ -24,31 +15,22 @@ class Assets {
   static const String mailIcon = "assets/images/mailIcon.png";
   static const String mapsIcon = "assets/images/mapsIcon.png";
 
-  static const String img1 = "assets/images/img1.jpg";
-  static const String img2 = "assets/images/img2.jpg";
   static const String img3 = "assets/images/img3.jpg";
   static const String img4 = "assets/images/img4.jpg";
-  static const String img5 = "assets/images/img5.jpg";
   static const String img6 = "assets/images/img6.png";
   static const String img7 = "assets/images/img7.jpg";
   static const String img8 = "assets/images/img8.jpg";
 
 
 
-  static const String offer1 = "assets/images/offer1.png";
   static const String offer2 = "assets/images/offer2.png";
   static const String offer3 = "assets/images/offer3.png";
-  static const String offer4 = "assets/images/offer4.png";
-  static const String offer5 = "assets/images/offer5.png";
-  static const String offer6 = "assets/images/offer6.png";
   static const String offer7 = "assets/images/offer7.png";
   static const String offer8 = "assets/images/offer8.png";
-  static const String offer9 = "assets/images/offer9.png";
 
 //Icons
   static const String facebookLogo = "assets/icons/fbIcon.png";
   static const String instaLogo = "assets/icons/instaIcon.png";
-  static const String messengerLogo = "assets/icons/messengerIcon.png";
   static const String twitterLogo = "assets/icons/twitterIcon.png";
   static const String youtubeLogo = "assets/icons/youtubeIcon.png";
   static const String minusLogo = "assets/icons/minus.png";
@@ -57,16 +39,12 @@ class Assets {
 
   static const String iconCar = "assets/icons/iconCar4.png";
   static const String iconHandHealth = "assets/icons/iconHandHealth.png";
-  static const String iconHandHome = "assets/icons/iconHandHome.png";
   static const String iconHandMoney = "assets/icons/iconHandMoney.png";
   static const String iconHandSafety = "assets/icons/iconHandSafety.png";
   static const String iconHandShake = "assets/icons/iconHandshake.png";
   static const String iconHomeLoan = "assets/icons/iconHomeLoan.png";
   static const String iconMen = "assets/icons/iconMen.png";
-  static const String iconMoneyPlant = "assets/icons/iconMoneyPlant.png";
-  static const String iconProgress = "assets/icons/iconProgress.png";
   static const String iconProperty = "assets/icons/iconProperty.png";
-  static const String iconSuitcase = "assets/icons/iconSuitcase.png";
   static const String iconOverdraft = "assets/icons/overdraft.png";
 
 
